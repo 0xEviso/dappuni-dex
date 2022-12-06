3fd00652246f5fa8316c35cb39ecb14b848b9cf0
Eviso <eviso@protonmail.com>
2022-12-06 09:50:24 +0700

    add symbol property to Token contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,5 +5,6 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Token {
-    string public name = "My token";
+    string public name = "Dapp University";
+    string public symbol = "DAPP";
 }
--- test/Token.js
+++ test/Token.js
@@ -3,13 +3,23 @@ const { ethers } = require('hardhat');
 
 describe('Token', () => {
 
-    it('has a name', async () => {
+    it('has correct name', async () => {
         // Fetch token from blockchain
         const Token = await ethers.getContractFactory('Token');
         let token = await Token.deploy();
         // Read token name
         const name = await token.name();
         // Check that name is correct
-        expect(name).to.equal('My token');
+        expect(name).to.equal('Dapp University');
+    });
+
+    it('has correct symbol', async () => {
+        // Fetch token from blockchain
+        const Token = await ethers.getContractFactory('Token');
+        let token = await Token.deploy();
+        // Read token symbol
+        const symbol = await token.symbol();
+        // Check that symbol is correct
+        expect(symbol).to.equal('DAPP');
     });
 });
