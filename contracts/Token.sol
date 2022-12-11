7b11c3246ed57337dbe1714dd1c1849ca39dbdf0
Eviso <eviso@protonmail.com>
2022-12-11 16:21:08 +0700

    add transferFrom function to token contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -40,15 +40,19 @@ contract Token {
         public
         returns (bool success)
     {
-        require(balanceOf[msg.sender] >= _value);
-        require(_to != address(0));
-
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-
-        emit Transfer(msg.sender, _to, _value);
+        _transfer(msg.sender, _to, _value);
 
         return true;
+    }
+
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        require(_to != address(0));
+        require(balanceOf[_from] >= _value);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(_from, _to, _value);
     }
 
     function approve(address _spender, uint _value)
@@ -59,6 +63,19 @@ contract Token {
 
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value)
+        public
+        returns(bool success)
+    {
+        require(allowance[_from][msg.sender] >= _value);
+
+        allowance[_from][msg.sender] -= _value;
+
+        _transfer(_from, _to, _value);
 
         return true;
     }
--- test/Token.js
+++ test/Token.js
@@ -134,4 +134,59 @@ describe('Token', () => {
             });
         });
     });
+
+    describe('Delegated Token Tranfers', () => {
+        let amount, transaction, result;
+
+        beforeEach(async() => {
+            amount = tokens('100');
+            transaction = await token.connect(deployer).approve(exchange.address, amount);
+            result = await transaction.wait();
+        });
+
+        describe('Success', () => {
+            beforeEach(async() => {
+                transaction = await token.connect(exchange)
+                    .transferFrom(deployer.address, receiver.address, amount);
+                result = await transaction.wait();
+            });
+
+            it('transfers token balances', async () => {
+                expect(await token.balanceOf(deployer.address)).to.equal(ethers.utils.parseEther('999900'));
+                expect(await token.balanceOf(receiver.address)).to.equal(amount);
+            });
+
+            it('resets the allowance', async () => {
+                expect(await token.allowance(deployer.address, exchange.address)).to.equal(0);
+            });
+
+            it('emits a Transfer event', async () => {
+                const event = result.events[0];
+                expect(event.event).to.equal('Transfer');
+
+                const args = event.args;
+                expect(args._from).to.equal(deployer.address);
+                expect(args._to).to.equal(receiver.address);
+                expect(args._value).to.equal(amount);
+            });
+        });
+
+        describe('Failure', () => {
+            it('rejects unauthorized spenders', async () => {
+                await expect(
+                    token.connect(receiver)
+                    .transferFrom(deployer.address, receiver.address, amount)
+                ).to.be.reverted;
+            });
+
+            it('rejects amount greater than authorized', async () => {
+                const invalidAmount = tokens('1000');
+
+                await expect(
+                    token.connect(exchange)
+                    .transferFrom(deployer.address, receiver.address, invalidAmount)
+                ).to.be.reverted;
+            });
+        });
+    });
 });
